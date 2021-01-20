940f6a683d2cf923311f4d8dec06d94d2db972e3
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2021-01-20 17:35:07 -0300

    Fix logotype header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,18 +5,21 @@ import logo from '../../assets/img/world.png'
 
 const HeaderWrapper = styled.header`
     background-color: #000;
-    height: 82px;
-    width: 100%;
+    height: 88px;
+    width: 100vw;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 
     display: flex;
     align-items: center;
+    overflow: hidden;
 `;
 
 const ImgWrapper = styled.img`
-    height: 100%;
-    margin-left: 53px;
-    
+    position: absolute;
+    width: 152px;
+    height: 139px;
+    left: 53px;
+    top: -28px;
 `;
 
 function Header() {
