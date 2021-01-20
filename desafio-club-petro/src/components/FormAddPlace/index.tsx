f5e8b9719b1f326d76cd26f2e639804c21d8bb27
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-01-20 14:51:32 -0300

    Configurando margin na logo

---
+++ desafio-club-petro/src/components/FormAddPlace/index.tsx
@@ -0,0 +1,9 @@
+const FormAddPlace = () => {
+  return (
+    <>
+      <h1>Form add place</h1>
+    </>
+  );
+};
+
+export default FormAddPlace;
--- desafio-club-petro/src/pages/Home.tsx
+++ desafio-club-petro/src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import FormAddPlace from "../components/FormAddPlace"
 import Header from "../components/Header"
 
 const Home = () => {
   return (
     <>
       <Header />
+      <FormAddPlace />
     </>
   )
 }
