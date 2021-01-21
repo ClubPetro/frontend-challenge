6b8513bbdd815d48b8f5df6fe4ad4dc7c0d09ec2
Renato Silva <orenatodos@gmail.com>
2021-01-21 17:08:49 -0300

    :lipstick: feat: cria estilos globais

--- challenge/src/App.tsx
+++ challenge/src/App.tsx
@@ -1,3 +1,10 @@
+import GlobalStyle from './styles/global';
+
 export default function App() {
-  return <h1>Lugares que eu quero conhecer</h1>;
+  return (
+    <>
+      <h1>Lugares que eu quero conhecer</h1>
+      <GlobalStyle />
+    </>
+  );
 }
