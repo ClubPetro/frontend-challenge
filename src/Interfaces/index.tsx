66a9d28f378812526c5e3f66517701629ebffbf1
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-01-24 10:29:29 -0300

    Refactor: Interfaces file

---
+++ src/Interfaces/index.tsx
@@ -0,0 +1,13 @@
+export interface ICountry {
+  translations: {
+    br: string;
+  };
+  flag: string;
+}
+
+export interface IGoal {
+  id?: string;
+  country: ICountry;
+  spot: string;
+  date: string;
+}
