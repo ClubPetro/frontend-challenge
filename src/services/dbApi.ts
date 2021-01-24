576c4ebfa3c786aa7660afb2abb3c7c28d240330
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-01-24 10:28:39 -0300

    Feat(dbApi): Database connection and services

---
+++ src/services/dbApi.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { IGoal } from '../Interfaces/index';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+const dbApi = {
+  async getGoals(): Promise<IGoal[]> {
+    return (await api.get('/goals')).data;
+  },
+  async createGoal(goal: IGoal): Promise<IGoal[]> {
+    return (await api.post('/goals', goal)).data;
+  },
+  async deleteGoal(id: string): Promise<IGoal[]> {
+    return (await api.delete(`/goals/${id}`)).data;
+  },
+};
+
+export default dbApi;
