49580d9876717aa53eeb4dae3344aaba445a8aca
alvaro bianor <bianor.alvaro12@gmail.com>
2021-05-25 21:51:04 -0300

    Created colors

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --background: rgba(240,242,245,1.0);
+    --darkGreen: rgba(0,108,24,1.0);
+    
+    --lightGreen: rgba(79,148,25,1.0);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 92.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+
+  }
+
+  body {
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, text-area, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+  
+`;
